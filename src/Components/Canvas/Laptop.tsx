39a4f3cc0820e967a253efec47d679657f1aa4cd
Mohamed Ibrahim <Mohamedibrahim208209@gmail.com>
2024-08-01 03:24:04 +0300

    last update

--- src/Components/Canvas/Laptop.tsx
+++ src/Components/Canvas/Laptop.tsx
@@ -1,63 +1,66 @@
-import { Suspense,useEffect,useState } from "react"
-import { Canvas } from "@react-three/fiber"
-import { useGLTF, Preload,OrbitControls } from "@react-three/drei"
-import { CanvasLoader } from "../CanvasLoader"
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Preload, OrbitControls, Center } from "@react-three/drei";
+import { CanvasLoader } from "../CanvasLoader";
 
 type LaptopProps = {
-  isMobile:boolean
-}
+  isMobile: boolean;
+};
 
-export const Laptop = ({isMobile}:LaptopProps ) => {
-  const LaptopModel = useGLTF("/Laptop/scene.gltf") 
+export const Laptop = ({ isMobile }: LaptopProps) => {
+  const LaptopModel = useGLTF("/Laptop/scene.gltf");
+  const scale = isMobile ? 0.8 : 1;
+  const position = isMobile ? [-2, -3.5, 1] : [-2, -4, 1.6];
+
   return (
-    <mesh>
+    <Center>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      
-      <primitive object={LaptopModel.scene}
-        scale={isMobile ? 0.8: 1}
 
-        position={isMobile?[-2,-3.5,1]: [-2,-4,1.6]}
-        rotation={[0,3,0]}
+      <primitive
+        object={LaptopModel.scene}
+        scale={scale}
+        position={position}
+        rotation={[0, 3, 0]}
       />
-
-    </mesh>
-  )
-}
-
+    </Center>
+  );
+};
 
 export const LaptopCanvas = () => {
-  const [isMobile, setIsMobile] =useState(false)
-  useEffect(()=>{
-    const mediaQuery = window.matchMedia("max-width:500px")
-    setIsMobile(mediaQuery.matches)
-    const handleMediaChange = (event:MediaQueryListEvent) =>{
-      setIsMobile(event.matches)
-    }
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
     mediaQuery.addEventListener("change", handleMediaChange);
-    return ()=> {
-      mediaQuery.removeEventListener("change",handleMediaChange)
-    }
-  },[])
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaChange);
+    };
+  }, []);
+
   return (
     <Canvas
-     frameloop="demand"
-     shadows
-     camera={{position: [20, 3, 5], fov: 25}} 
-     gl={{preserveDrawingBuffer:true}}
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader/>}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
         <Laptop isMobile={isMobile} />
-        
-
       </Suspense>
-       <Preload all />
+      <Preload all />
     </Canvas>
-  )
-}
+  );
+};
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,42 +1,32 @@
-import { styles } from "../styles"
-import { motion } from "framer-motion"
-import {  LaptopCanvas } from "./Canvas/Laptop"
+import { styles } from "../styles";
+import { LaptopCanvas } from "./Canvas/Laptop";
 
 export const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto" >
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px]
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px]
       max-w-7xl mx-auto flex flex-row items-start gap-5 
-      `}>
+      `}
+      >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-sky-700 "/>
-          <div className="w-1 sm:80 h-44 Cyan-gradient"/>
-          </div>
-          <div>
-           <h1 className={`${styles.heroHeadText} text-white`}>Hello, I'm
-            <span className="text-[#0284c7]"> Mohamed
-            </span>
-           </h1>
-           <p className={`${styles.heroSubText} mt-2 text-white`}>
-            a web developer
-
-           </p>
-          </div>
-      </div>
-      <LaptopCanvas />
-      <div className="absolute xs:bottom-10 bottom-[0px] w-full flex justify-center items-center">
-       <a title="scroll down" href="#about">
-        <div className="w-[25px] h-[50px] rounded-3xl border-4 border-[#8422c5]
-        flex justify-center items-start p-0
-        ">
-          <motion.div animate={{ y:[0,10,0]}}
-          transition={{duration:1.5, repeat:Infinity, repeatType:"loop"}}
-          className="w-2 h-6 rounded-full bg-lime-600 mt-1"
-           />
-
+          <div className="w-5 h-5 rounded-full bg-sky-700 " />
+          <div className="w-1 sm:80 h-44 Cyan-gradient" />
         </div>
-       </a>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hello, I'm
+            <span className="text-[#0284c7]"> Mohamed</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white`}>
+            a web developer
+          </p>
+        </div>
+      </div>
+
+      <div className="absolute bottom-[-10%] left-[-20%] h-[550px] w-full">
+        <LaptopCanvas />
       </div>
     </section>
-  )
-}
+  );
+};
